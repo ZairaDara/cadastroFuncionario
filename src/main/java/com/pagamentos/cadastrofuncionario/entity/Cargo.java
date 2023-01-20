4df92f50eb02b5c7d3bc0e8f68acca21fa8cf17c
julyomaia <60893300+julyomaia@users.noreply.github.com>
2023-01-20 17:01:29 -0300

    Ajustes nas tabelas

--- src/main/java/com/pagamentos/cadastrofuncionario/entity/Cargo.java
+++ src/main/java/com/pagamentos/cadastrofuncionario/entity/Cargo.java
@@ -1,17 +1,23 @@
 package com.pagamentos.cadastrofuncionario.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
+@Getter
+@Setter
 @Entity
 public class Cargo {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "idcargo")
+    private Long idCargo;
     private String descricaoCargo;
     private BigDecimal salarioBase;
+
+    @OneToMany(mappedBy = "cargo",fetch = FetchType.LAZY)
+    private Set<Funcionario> listaFuncionarios;
 }
--- src/main/java/com/pagamentos/cadastrofuncionario/entity/Funcionario.java
+++ src/main/java/com/pagamentos/cadastrofuncionario/entity/Funcionario.java
@@ -2,18 +2,23 @@ package com.pagamentos.cadastrofuncionario.entity;
 
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.security.PrivateKey;
 import java.util.Date;
 //comentario para testar
+@Getter
+@Setter
 @Entity
 public class Funcionario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long idFuncionario;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "idcargo")
     private Cargo cargo;
 
     private String nome;
