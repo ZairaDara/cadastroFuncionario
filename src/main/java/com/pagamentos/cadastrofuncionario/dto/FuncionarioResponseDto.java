0b81f3d9be240d942f82dda819e77437df32d14c
julyomaia <60893300+julyomaia@users.noreply.github.com>
2023-01-25 11:48:40 -0300

    criação do controller funcionarios

---
+++ src/main/java/com/pagamentos/cadastrofuncionario/dto/FuncionarioResponseDto.java
@@ -0,0 +1,14 @@
+package com.pagamentos.cadastrofuncionario.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class FuncionarioResponseDto {
+    private Long idFuncionario;
+}
